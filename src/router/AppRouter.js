3c70a643c785973a1a7284f7850c244ebaea8b99
daviddexteel <dsilvestri@dexteel.com>
2024-05-30 20:12:11 -0300

    chat components and chat page

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
-import { ChatPage } from '../pages/ChatPage';
+// import { ChatPage } from '../pages/ChatPage';
 import { AuthRouter } from './AuthRouter';
 
 export const AppRouter = () => {
@@ -9,8 +9,9 @@ export const AppRouter = () => {
     <Router>
       <Routes>
         <Route path='/auth/*' element={<AuthRouter />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/*" element={<Navigate to="/chat" replace />} />
+        {/* <Route path="/chat" element={<ChatPage />} /> */}
+        {/* <Route path="/*" element={<Navigate to="/chat" replace />} /> */}
+        <Route path="/*" element={<Navigate to="/auth" replace />} />
       </Routes>
     </Router>
   );
