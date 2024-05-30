e10fe9f345e7f3329e517f2389724b5e604639d2
daviddexteel <dsilvestri@dexteel.com>
2024-05-30 20:04:03 -0300

    Login and Register page

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { LoginPage } from '../pages/LoginPage';
-import { RegisterPage } from '../pages/RegisterPage';
+
 import { ChatPage } from '../pages/ChatPage';
+import { AuthRouter } from './AuthRouter';
 
 export const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
+        <Route path='/auth/*' element={<AuthRouter />} />
         <Route path="/chat" element={<ChatPage />} />
         <Route path="/*" element={<Navigate to="/chat" replace />} />
-
       </Routes>
     </Router>
   );
