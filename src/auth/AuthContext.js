d491eb0ce72ebb8a02abe956394f80aa4e7d00ad
daviddexteel <dsilvestri@dexteel.com>
2024-05-31 18:53:50 -0300

    LoginPage: onChange, toggleCheck, onSubmit

---
+++ src/auth/AuthContext.js
@@ -0,0 +1,44 @@
+
+
+import React, { useCallback, useState } from 'react'
+import { createContext } from "react";
+
+export const AuthContext = createContext();
+
+const initialState = {
+    uid: null,
+    checking: true,
+    logged: false,
+    name: null,
+};
+
+export const AuthProvider= ({children}) => {
+  
+  const [auth, setAuth] = useState(initialState);
+  const login = (email,password)=> {
+
+  }
+
+  const register = (name,email,password)=>{
+
+  }
+
+  const verifyToken = useCallback( () => {
+
+  },[])
+
+  const logout = ()=> {
+
+  }
+
+  return (
+    <AuthContext.Provider value={{
+        login,
+        register,
+        verifyToken,
+        logout
+    }}>
+        {children}
+    </AuthContext.Provider>
+  )
+}
