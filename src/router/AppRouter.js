8208d99015bcbddede1d04937c049e47652cd533
daviddexteel <dsilvestri@dexteel.com>
2024-06-01 08:38:22 -0300

    PrivateRoute, PublicRoute ready

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,31 +1,42 @@
 import React, { useContext, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-
-// import { ChatPage } from '../pages/ChatPage';
-import { AuthRouter } from './AuthRouter';
 import { AuthContext } from '../auth/AuthContext';
 
-export const AppRouter = () => {
+import { ChatPage } from '../pages/ChatPage';
+import { AuthRouter } from './AuthRouter';
+import { PublicRoute } from './PublicRoute';
+import { PrivateRoute } from './PrivateRoute';
+// import { PrivateRoute } from './PrivateRoute';
 
-  const { auth, verifyToken} = useContext(AuthContext)
-  
+export const AppRouter = () => {
+  const { auth, verifyToken } = useContext(AuthContext);
+
   useEffect(() => {
     verifyToken();
-  }, [verifyToken])
-  
-  
-  if (auth.checking){
-    return <h1>Loading...</h1>
+  }, [verifyToken]);
+
+  if (auth.checking) {
+    return <h1>Espere por favor</h1>;
   }
 
   return (
     <Router>
-      <Routes>
-        <Route path='/auth/*' element={<AuthRouter />} />
-        {/* <Route path="/chat" element={<ChatPage />} /> */}
-        {/* <Route path="/*" element={<Navigate to="/chat" replace />} /> */}
-        <Route path="/*" element={<Navigate to="/auth" replace />} />
-      </Routes>
+      <div>
+        <Routes>
+          
+          <Route path="/auth/*" element={
+            <PublicRoute isAuthenticated={auth.logged}>
+              <AuthRouter />
+            </PublicRoute>
+          } />
+          <Route path="/" element={
+            <PrivateRoute isAuthenticated={auth.logged}>
+              <ChatPage />
+            </PrivateRoute>
+          } />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </div>
     </Router>
   );
 };
